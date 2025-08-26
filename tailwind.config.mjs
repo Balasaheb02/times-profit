b6d43e95b0115bc9a5cbaff4dda7642dbcbbb533
Balasaheb Thakare <thakarebalasaheb02@gmail.com>
2025-08-27 04:26:07 +0530

    refactor: migrate PostCSS configuration to .mjs format and add Tailwind CSS configuration

---
+++ tailwind.config.mjs
@@ -0,0 +1,116 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          "100%": { transform: "translate(-50%)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 5s ease-in-out infinite",
+      },
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+        },
+        custom: {
+          gray: {
+            50: "#00000005",
+            100: "#FFFFFF40",
+            200: "#F5F5F5",
+            300: "#808080",
+          },
+          dim: "#D9D9D9",
+          green: "#37A600",
+          red: "#D90000",
+        },
+      },
+      fontFamily: {
+        body: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+      borderWidth: {
+        DEFAULT: "1px",
+        0: "0",
+        2: "2px",
+        3: "3px",
+        4: "4px",
+        6: "6px",
+        8: "8px",
+      },
+      minHeight: {
+        "0": "0px",
+        "full": "100%",
+        "screen": "100vh",
+        "svh": "100svh",
+        "lvh": "100lvh",
+        "dvh": "100dvh",
+        "min": "min-content",
+        "max": "max-content",
+        "fit": "fit-content",
+      },
+      minWidth: {
+        "0": "0px",
+        "full": "100%",
+        "min": "min-content",
+        "max": "max-content",
+        "fit": "fit-content",
+      },
+    },
+  },
+  plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+}
+
+export default config
