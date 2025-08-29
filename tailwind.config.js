9c76e375c4594ca168c8b5e3209f449cb4aae210
Balasaheb Thakare <thakarebalasaheb02@gmail.com>
2025-08-29 11:46:50 +0530

    fix: update PostCSS configuration to use array syntax; add missing mdx file extension in Tailwind CSS config; create initial Vercel configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
